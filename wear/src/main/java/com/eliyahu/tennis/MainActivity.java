ec938b7cd9faf7c7835fe48d51337575c1ccb230
egkoppel <egluschovekoppel1@gmail.com>
2016-12-14 18:42:43 +0000

    Read sensors - Accelerometer - Heart rate

--- wear/src/main/java/com/eliyahu/tennis/MainActivity.java
+++ wear/src/main/java/com/eliyahu/tennis/MainActivity.java
@@ -1,8 +1,14 @@
 package com.eliyahu.tennis;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,14 +16,21 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class MainActivity extends WearableActivity {
+public class MainActivity extends WearableActivity implements SensorEventListener {
 
+    private SensorManager sensorManager;
+    double ax,ay,az;   // these are the acceleration in x,y and z axis
+    float heart;
+    private static final String TAG = "Sensors";
     private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
             new SimpleDateFormat("HH:mm", Locale.US);
 
     private BoxInsetLayout mContainerView;
     private TextView mTextView;
     private TextView mClockView;
+
+    Handler h = new Handler();
+    int delay = 500; //milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,18 @@ public class MainActivity extends WearableActivity {
         mContainerView = (BoxInsetLayout) findViewById(R.id.container);
         mTextView = (TextView) findViewById(R.id.text);
         mClockView = (TextView) findViewById(R.id.clock);
+
+        sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE), SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        h.postDelayed(new Runnable(){
+            public void run(){
+                //do something
+                read();
+            }
+        }, delay);
     }
 
     @Override
@@ -60,5 +85,30 @@ public class MainActivity extends WearableActivity {
             mTextView.setTextColor(getResources().getColor(android.R.color.black));
             mClockView.setVisibility(View.GONE);
         }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+            ax=event.values[0];
+            ay=event.values[1];
+            az=event.values[2];
+        } else if (event.sensor.getType()==Sensor.TYPE_HEART_RATE){
+            heart = event.values[0];
+        }
+        mTextView.setText("X: " + String.valueOf(ax) + "\nY: " + String.valueOf(ay) + "\nZ:" + String.valueOf(az) + "\nHeart: " + String.valueOf(heart));
+    }
+    public void read() {
+        Log.i(TAG, "Accel:\nX: " + String.valueOf(ax) + "\nY: " + String.valueOf(ay) + "\nZ: " + String.valueOf(az) + "\n\nHeart rate: " + String.valueOf(heart));
+        h.postDelayed(new Runnable(){
+            public void run(){
+                //do something
+                read();
+            }
+        }, delay);
     }
 }
